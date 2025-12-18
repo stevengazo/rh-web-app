b18db633ed0f4db87e476659dd1a5bf505edd13c
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2025-12-17 21:07:28 -0600

    Init Project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className=''>
+      <h4 className=' text-2xl'>
+        Hello
+      </h4>
+    </div>
+    </>
+  )
+}
+
+export default App
