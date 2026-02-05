a67f7a0e6739b270b4712fb4ed26e5b9a8661bb2
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2026-02-05 14:35:48 -0600

    data

--- src/Components/atoms/PayrollRow.jsx
+++ src/Components/atoms/PayrollRow.jsx
@@ -1,12 +1,9 @@
 import { useState, useMemo } from 'react';
 
-const PayrollRow = ({ employee, PayrollData , onChanged, salary }) => {
-
+const PayrollRow = ({ employee, PayrollData, onChanged, salary }) => {
   /// Print in console
-  console.log(employee)
-  console.table(PayrollData)
+  console.table(PayrollData);
 
-  
   if (!PayrollData) {
     return (
       <tr className="bg-red-50 text-red-600">
@@ -20,7 +17,7 @@ const PayrollRow = ({ employee, PayrollData , onChanged, salary }) => {
   // ========================
   // Cálculos base
   // ========================
-  const salarioMensual = PayrollData.monthlySalary
+  const salarioMensual = PayrollData.monthlySalary;
 
   const salarioQuincenal = salarioMensual / 2;
   const salarioHora = salarioMensual / 30 / 8;
@@ -43,7 +40,7 @@ const PayrollRow = ({ employee, PayrollData , onChanged, salary }) => {
   // Montos calculados
   // ========================
   const montoExtras = extras * (salarioHora * 1.5);
-  const montoFeriados = feriados * ( (salarioHora * 8 ) * 2);
+  const montoFeriados = feriados * (salarioHora * 8 * 2);
   const montoExtrasFeriado = extrasFeriado * salarioHora * 2.5;
   const deducciones = (incCCSS + incINS + ausencias) * salarioDia;
 
@@ -83,80 +80,145 @@ const PayrollRow = ({ employee, PayrollData , onChanged, salary }) => {
   // ========================
   return (
     <tr className="hover:bg-slate-50 transition">
-      <td className="p-1 border border-gray-200 text-left font-medium">
+      {/* Empleado */}
+      <td className="p-2 border hover:bg-blue-100 border-gray-200 text-left font-medium">
         {employee.firstName} {employee.lastName}
       </td>
 
-      <td className="p-1 border border-gray-200 text-right">{formatMoney(salarioMensual)}</td>
-      <td className="p-1 border border-gray-200 text-right">{formatMoney(salarioQuincenal)}</td>
-      <td className="p-1 border border-gray-200 text-right">{formatMoney(salarioHora)}</td>
-
-      <td className="border border-gray-200 text-right">
-        <input type="number" step="0.01" min={0} value={extras}
+      {/* Salario Mensual */}
+      <td className="p-2 border hover:bg-blue-100 border-gray-200 text-right">
+        {formatMoney(salarioMensual)}
+      </td>
+      {/* Salario Quincenal */}
+      <td className="p-2 border hover:bg-blue-100 border-gray-200 text-right">
+        {formatMoney(salarioQuincenal)}
+      </td>
+      {/* Salario Hora */}
+      <td className="p-2 border hover:bg-blue-100 border-gray-200 text-right">
+        {formatMoney(salarioHora)}
+      </td>
+      {/* Cantidad Horas Extras */}
+      <td className="border border-gray-200 hover:bg-blue-100 text-right">
+        <input
+          type="number"
+          step="0.01"
+          min={0}
+          value={extras}
           onChange={(e) => setExtras(parseFloat(e.target.value) || 0)}
-          className="input" />
+          className="input"
+        />
       </td>
-      <td className="p-1 border border-gray-200 text-right">{formatMoney(montoExtras)}</td>
-
-      <td className="border border-gray-200 text-right">
-        <input type="number" step="0.01" min={0} value={feriados}
+      {/* Monto Horas Extras */}
+      <td className="p-2 border hover:bg-blue-100 border-gray-200 text-right">
+        {formatMoney(montoExtras)}
+      </td>
+      {/* Dias Feriados */}
+      <td className="border border-gray-200 hover:bg-blue-100 text-right">
+        <input
+          type="number"
+          step="0.01"
+          min={0}
+          value={feriados}
           onChange={(e) => setFeriados(parseFloat(e.target.value) || 0)}
-          className="input" />
+          className="input"
+        />
       </td>
-      <td className="p-1 border border-gray-200 text-right">{formatMoney(montoFeriados)}</td>
-
-      <td className="border border-gray-200 text-right">
-        <input type="number" step="0.01" min={0} value={extrasFeriado}
+      {/* Monto de Feriados */}
+      <td className="p-2 border hover:bg-blue-100 border-gray-200 text-right">
+        {formatMoney(montoFeriados)}
+      </td>
+      {/* Cantidad Extras Feriado */}
+      <td className="border border-gray-200 hover:bg-blue-100 text-right">
+        <input
+          type="number"
+          step="0.01"
+          min={0}
+          value={extrasFeriado}
           onChange={(e) => setExtrasFeriado(parseFloat(e.target.value) || 0)}
-          className="input" />
+          className="input"
+        />
       </td>
-      <td className="p-1 border border-gray-200 text-right">{formatMoney(montoExtrasFeriado)}</td>
-
-      <td className="border border-gray-200 text-right">
-        <input type="number" step="0.01" min={0} value={retroactivo}
+      {/* Monto Extras Feriado */}
+      <td className="p-2 border hover:bg-blue-100 border-gray-200 text-right">
+        {formatMoney(montoExtrasFeriado)}
+      </td>
+      {/* Monto Retroactivo */}
+      <td className="border border-gray-200 hover:bg-blue-100 text-right">
+        <input
+          type="number"
+          step="0.01"
+          min={0}
+          value={retroactivo}
           onChange={(e) => setRetroactivo(parseFloat(e.target.value) || 0)}
-          className="input" />
+          className="input"
+        />
       </td>
-
-      <td className="border border-gray-200 text-right">
-        <input type="number" step="0.01" min={0} value={bonos}
+      {/* Monto Feriado */}
+      <td className="border border-gray-200 hover:bg-blue-100 text-right">
+        <input
+          type="number"
+          step="0.01"
+          min={0}
+          value={bonos}
           onChange={(e) => setBonos(parseFloat(e.target.value) || 0)}
-          className="input" />
+          className="input"
+        />
       </td>
-
-      <td className="border border-gray-200 text-right">
-        <input type="number" step="0.01" min={0} value={comisiones}
+      {/* Monto Comisiones */}
+      <td className="border border-gray-200 hover:bg-blue-100 text-right">
+        <input
+          type="number"
+          step="0.01"
+          min={0}
+          value={comisiones}
           onChange={(e) => setComisiones(parseFloat(e.target.value) || 0)}
-          className="input" />
+          className="input"
+        />
       </td>
+      {/* Cantidad Incapacidad CCSS */}
 
-      <td className="border border-gray-200 text-right">
-        <input type="number" step="0.01" min={0} value={incCCSS}
+      <td className="border border-gray-200 hover:bg-blue-100 text-right">
+        <input
+          type="number"
+          step="0.01"
+          min={0}
+          value={incCCSS}
           onChange={(e) => setIncCCSS(parseFloat(e.target.value) || 0)}
-          className="input" />
+          className="input"
+        />
       </td>
-
-      <td className="border border-gray-200 text-right">
-        <input type="number" step="0.01" min={0} value={incINS}
+      {/* Cantidad Incapacidad INS */}
+      <td className="border border-gray-200 hover:bg-blue-100 text-right">
+        <input
+          type="number"
+          step="0.01"
+          min={0}
+          value={incINS}
           onChange={(e) => setIncINS(parseFloat(e.target.value) || 0)}
-          className="input" />
+          className="input"
+        />
       </td>
-
-      <td className="border border-gray-200 text-right">
-        <input type="number" step="0.01" min={0} value={ausencias}
+      {/* Cantidad Ausencias */}
+      <td className="border border-gray-200 hover:bg-blue-100 text-right">
+        <input
+          type="number"
+          step="0.01"
+          min={0}
+          value={ausencias}
           onChange={(e) => setAusencias(parseFloat(e.target.value) || 0)}
-          className="input" />
+          className="input"
+        />
       </td>
-
-      <td className="p-1 border border-gray-200 text-right font-semibold">
+      {/* Monto Salario Bruto */}
+      <td className="p-2 border hover:bg-blue-100 border-gray-200 text-right font-semibold">
         {formatMoney(salarioBruto)}
       </td>
-
-      <td className="p-1 border border-gray-200 text-right text-red-600">
+      {/* Monto Deducciones */}
+      <td className="p-2 border hover:bg-blue-100 border-gray-200 text-right text-red-600">
         {formatMoney(deducciones)}
       </td>
-
-      <td className="p-1 border border-gray-200 text-right font-bold text-emerald-600">
+      {/* Monto A Pagar */}
+      <td className="p-2 border hover:bg-blue-100 border-gray-200 text-right font-bold text-emerald-600">
         {formatMoney(netoPagar)}
       </td>
     </tr>
