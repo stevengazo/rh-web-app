24c0603121b8516191c662bd0ddd85afb3b3ccaf
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2026-02-14 16:45:02 -0600

    Feat: ngnix test

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class ApiClient {
   constructor() {
     this.client = axios.create({
-      baseURL: import.meta.env.VITE_API_URL,
+       baseURL: "/api",
       headers: {
         'Content-Type': 'application/json',
       },
